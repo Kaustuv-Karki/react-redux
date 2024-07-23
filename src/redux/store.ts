70fdff30baa879b348e39a736bc623a95fbf3f49
Kaustuv <Kaustuv-Karki>
2024-07-24 01:41:53 +0545

    redux bugfix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {todoReducer} from './slices/todoSlice'
+import { configureStore, Store } from "@reduxjs/toolkit";
+import {todoReducer} from './slices/todoSlice.ts'
 
-const store = configureStore({
+const store: Store = configureStore({
     reducer: {
         // Add reducers here
         todos: todoReducer
