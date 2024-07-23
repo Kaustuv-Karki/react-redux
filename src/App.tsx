e5a3ca2599ea6fc817d42337d1e2dc5582c0f8ac
Kaustuv <Kaustuv-Karki>
2024-07-24 01:39:53 +0545

    Simple implemtation of redux slice and reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { addTodo } from './redux/slices/todoSlice'
 
 function App() {
   const {allTodos} = useSelector((state: any) => state.todos)
+  const disptach = useDispatch()
   console.log("This is the data",allTodos)
+  const [todo, setTodo] = useState('')
+
+  const handleAddTodo = () => {
+  disptach(addTodo({id: allTodos.length + 1, todo}))
+  setTodo('')
+    console.log("This is the todo",todo)
+  }
 
   return (
     <>
-
+      {
+        allTodos.map((todo: any) => (
+          <div key={todo.id}>
+            <h1>{todo.todo}</h1>
+          </div>
+      )
+    )}
+    <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter your todo" />
+    <button onClick={handleAddTodo}>Add Todo</button>
     </>
   )
 }
